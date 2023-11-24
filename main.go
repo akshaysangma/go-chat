3ee09f33d216f38d54e488cfd3b267dec0b34873
akshaysangma <akshay.sangma@outlook.com>
2023-11-24 19:55:25 +0530

    Function Decomposition

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 )
 
 const (
-	PORT          = "8125"
-	MSGBUFFERSIZE = 512
-	DEBUGMODE     = false
-	RATELIMIT     = 1.0
-	STRIKECOUNT   = 10
-	BANTIMEOUT    = 10.0 * 60.0
+	Port          = "8125"
+	MsgBufferSize = 512
+	DebugMode     = false
+	RateLimit     = time.Second
+	StrikeCount   = 10
+	BanTimeout    = 10 * time.Minute
 )
 
 func getSensitive(s string) string {
-	if !DEBUGMODE {
+	if !DebugMode {
 		return "[REDACTED]"
 	}
 	return s
@@ -27,9 +27,9 @@ func getSensitive(s string) string {
 type MessageType int
 
 const (
-	clientConnected MessageType = iota + 1
-	clientDisconnected
-	newMessage
+	ClientConnected MessageType = iota + 1
+	ClientDisconnected
+	NewMessage
 )
 
 type Message struct {
@@ -41,94 +41,21 @@ type Message struct {
 type Client struct {
 	conn              net.Conn
 	lastMessageSentAt time.Time
-	strikecount       int
+	strikeCount       int
 }
 
 func server(messages chan Message) {
 	clients := make(map[string]*Client)
 	bannedClients := make(map[string]time.Time)
-	for {
-		msg := <-messages
+
+	for msg := range messages {
 		switch msg.msgType {
-		case clientConnected:
-			tcpIP := msg.conn.RemoteAddr().(*net.TCPAddr).IP.String()
-			now := time.Now()
-			bannedAt, isbanned := bannedClients[tcpIP]
-			if isbanned {
-				if now.Sub(bannedAt).Seconds() > BANTIMEOUT {
-					delete(bannedClients, tcpIP)
-					clients[msg.conn.RemoteAddr().String()].strikecount = 0
-					isbanned = false
-				}
-			}
-			if !isbanned {
-				log.Printf("Client %s connected\n", getSensitive(msg.conn.RemoteAddr().String()))
-				welcomemsg := fmt.Sprintf(
-					"%s has join the chat\n",
-					getSensitive(msg.conn.RemoteAddr().String()),
-				)
-				msg.conn.Write([]byte(welcomemsg))
-				clients[msg.conn.RemoteAddr().String()] = &Client{
-					conn:              msg.conn,
-					lastMessageSentAt: time.Now(),
-				}
-			} else {
-				msg.conn.Write([]byte("You are currently banned!"))
-				msg.conn.Close()
-			}
-		case clientDisconnected:
-			log.Printf("Client %s disconnected\n", getSensitive(msg.conn.RemoteAddr().String()))
-			delete(clients, msg.conn.RemoteAddr().String())
-			msg.conn.Close()
-		case newMessage:
-			addr := msg.conn.RemoteAddr().String()
-			now := time.Now()
-			if clients[addr] != nil {
-				if now.Sub(clients[addr].lastMessageSentAt).Seconds() > RATELIMIT {
-					if utf8.ValidString(msg.text) {
-						clients[addr].lastMessageSentAt = now
-						clients[addr].strikecount = 0
-						log.Printf(
-							"Client %s sent message %s",
-							getSensitive(msg.conn.RemoteAddr().String()),
-							msg.text,
-						)
-						for _, client := range clients {
-							if client.conn.RemoteAddr().String() == addr {
-								continue
-							}
-							_, err := client.conn.Write([]byte(msg.text))
-							if err != nil {
-								log.Printf(
-									"Unable to write to %s, err : %s/n",
-									getSensitive(client.conn.RemoteAddr().String()),
-									getSensitive(err.Error()),
-								)
-								messages <- Message{conn: client.conn, msgType: clientDisconnected}
-							}
-						}
-					} else {
-						clients[addr].strikecount++
-					}
-				} else {
-					clients[addr].strikecount++
-				}
-				if clients[addr].strikecount > STRIKECOUNT {
-					if tcpAddr, ok := clients[addr].conn.RemoteAddr().(*net.TCPAddr); ok {
-						log.Printf(
-							"Client %s banned for too much message",
-							getSensitive(tcpAddr.IP.String()),
-						)
-						bannedClients[tcpAddr.IP.String()] = now
-						msg.conn.Write([]byte("You are banned"))
-						clients[addr].conn.Close()
-						delete(clients, addr)
-					}
-				}
-			} else {
-				msg.conn.Write([]byte("You are banned"))
-				msg.conn.Close()
-			}
+		case ClientConnected:
+			handleClientConnected(msg, clients, bannedClients)
+		case ClientDisconnected:
+			handleClientDisconnected(msg, clients)
+		case NewMessage:
+			handleNewMessage(msg, clients, bannedClients)
 		}
 	}
 }
@@ -136,9 +63,9 @@ func server(messages chan Message) {
 func connect(conn net.Conn, messages chan Message) {
 	messages <- Message{
 		conn:    conn,
-		msgType: clientConnected,
+		msgType: ClientConnected,
 	}
-	buffer := make([]byte, MSGBUFFERSIZE)
+	buffer := make([]byte, MsgBufferSize)
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
@@ -149,35 +76,132 @@ func connect(conn net.Conn, messages chan Message) {
 			)
 			messages <- Message{
 				conn:    conn,
-				msgType: clientDisconnected,
+				msgType: ClientDisconnected,
 			}
 			return
 		}
 		messages <- Message{
 			conn:    conn,
-			msgType: newMessage,
+			msgType: NewMessage,
 			text:    string(buffer[:n]),
 		}
 	}
 }
 
 func main() {
-	// TODO : Environemnt Variables
-	// TODO : replace log with slog
-
-	ln, err := net.Listen("tcp", ":"+PORT)
+	ln, err := net.Listen("tcp", ":"+Port)
 	if err != nil {
-		log.Fatalf("Unable to start TCP listener at port %s, err : %s\n", PORT, err)
+		log.Fatalf("Unable to start TCP listener at port %s, err : %s\n", Port, err)
 	}
 
 	log.Printf("Server successfully listening at %s", ln.Addr())
 	messages := make(chan Message)
 	go server(messages)
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Printf("Failed to accept conn, err %s\n", err)
+			continue
 		}
 		go connect(conn, messages)
+	}
+}
+
+func handleClientConnected(
+	msg Message,
+	clients map[string]*Client,
+	bannedClients map[string]time.Time,
+) {
+	tcpIP := msg.conn.RemoteAddr().(*net.TCPAddr).IP.String()
+	now := time.Now()
+	bannedAt, isBanned := bannedClients[tcpIP]
+	if isBanned && now.Sub(bannedAt) > BanTimeout {
+		delete(bannedClients, tcpIP)
+		clients[msg.conn.RemoteAddr().String()].strikeCount = 0
+		isBanned = false
+	}
+
+	if !isBanned {
+		log.Printf("Client %s connected\n", getSensitive(msg.conn.RemoteAddr().String()))
+		welcomeMsg := fmt.Sprintf(
+			"%s has joined the chat\n",
+			getSensitive(msg.conn.RemoteAddr().String()),
+		)
+		msg.conn.Write([]byte(welcomeMsg))
+		clients[msg.conn.RemoteAddr().String()] = &Client{
+			conn:              msg.conn,
+			lastMessageSentAt: time.Now(),
+		}
+	} else {
+		msg.conn.Write([]byte("You are currently banned!"))
+		msg.conn.Close()
+	}
+}
+
+func handleClientDisconnected(msg Message, clients map[string]*Client) {
+	log.Printf("Client %s disconnected\n", getSensitive(msg.conn.RemoteAddr().String()))
+	delete(clients, msg.conn.RemoteAddr().String())
+	msg.conn.Close()
+}
+
+func handleNewMessage(msg Message, clients map[string]*Client, bannedClients map[string]time.Time) {
+	addr := msg.conn.RemoteAddr().String()
+	now := time.Now()
+	client, exists := clients[addr]
+	if !exists {
+		msg.conn.Write([]byte("You are banned"))
+		msg.conn.Close()
+		return
+	}
+
+	if now.Sub(client.lastMessageSentAt) > RateLimit {
+		if utf8.ValidString(msg.text) {
+			client.lastMessageSentAt = now
+			client.strikeCount = 0
+			log.Printf("Client %s sent message: %s", getSensitive(addr), msg.text)
+			broadcastMessage(addr, msg.text, clients)
+		} else {
+			client.strikeCount++
+		}
+	} else {
+		client.strikeCount++
+	}
+
+	if client.strikeCount > StrikeCount {
+		handleBan(client, addr, bannedClients, clients)
+	}
+}
+
+func broadcastMessage(senderAddr, text string, clients map[string]*Client) {
+	for _, client := range clients {
+		if client.conn.RemoteAddr().String() == senderAddr {
+			continue
+		}
+		_, err := client.conn.Write([]byte(text))
+		if err != nil {
+			log.Printf(
+				"Unable to write to %s, err: %s\n",
+				getSensitive(client.conn.RemoteAddr().String()),
+				getSensitive(err.Error()),
+			)
+			client.conn.Close()
+			delete(clients, client.conn.RemoteAddr().String())
+		}
+	}
+}
+
+func handleBan(
+	client *Client,
+	addr string,
+	bannedClients map[string]time.Time,
+	clients map[string]*Client,
+) {
+	if tcpAddr, ok := client.conn.RemoteAddr().(*net.TCPAddr); ok {
+		log.Printf("Client %s banned for excessive messaging", getSensitive(tcpAddr.IP.String()))
+		bannedClients[tcpAddr.IP.String()] = time.Now()
+		client.conn.Write([]byte("You are banned"))
+		client.conn.Close()
+		delete(clients, addr)
 	}
 }
